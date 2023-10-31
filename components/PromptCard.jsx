e4ed1b9d16f5dad0fc45cdf2dba2379a153f95f0
JM Fantin <jmfantin@outlook.com>
2023-10-31 17:08:10 -0300

    i need a fxxxing break

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,3 +1,22 @@
-export function PromptCard() {
-  return <div>PromptCard</div>;
+'use client'; //CLIENT COMPONENT
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
+
+export function PromptCard({ post, handleTagClick, handleEdit, handleDelete }) {
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <Image
+          src={post.creator.image}
+          alt="user image"
+          width={40}
+          height={40}
+          className="rounded-full object-contain"
+        />
+      </div>
+    </div>
+  );
 }
